351df92f9180f6cd0239075f34e4e27beb7242c1
Reechee <yangrichie528@gmail.com>
2024-07-20 16:40:15 +1000

    finish themes and more styling

--- popup.js
+++ popup.js
@@ -1,22 +1,33 @@
+let rootElement = document.querySelector(":root");
+let screens = document.querySelectorAll(".screens");
+
 // THEMES
 
-let rootElement = document.querySelector(":root");
+let themeButton = document.querySelector(".theme-button");
 let currentTheme = 0;
-let themeKeys = ["--base", "--mantle", "--crust", "--surface", "--green", "--red", "--test", "--subtext"];
+let themeName = ["light", "dark"];
+let themeKeys = ["--base", "--mantle", "--crust", "--surface", "--green", "--red", "--text", "--subtext"];
 let lightMode = ["#eff1f5", "#e6e9ef", "#dce0e8", "#acb0be", "#a6d189", "#e78284", "#4c4f69", "#8f91a0"];
 let darkMode = ["#1e1e2e", "#181825", "#11111b", "#313244", "#a6d189", "#e78284", "#cdd6f4", "#98a0bc"];
 
 function displayTheme() {
     for (let i = 0; i < themeKeys.length; i++) {
-        rootElement.setProperty(
+        rootElement.style.setProperty(
             themeKeys[i],
             (currentTheme) ? darkMode[i] : lightMode[i]
         );
     }
+    themeButton.textContent = themeName[currentTheme];
 }
 function toggleTheme() {
-    currentTheme = !currentTheme;
+    currentTheme = (currentTheme) ? 0 : 1;
     displayTheme();
 }
 
+themeButton.onclick = function() {
+    toggleTheme();
+}
+
 displayTheme();
+
+// SNAKE
